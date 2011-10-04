cc1f48f0df767f6f496e1a8369e08d68e4ba5e0c
Will <me@willcannings.com>
2011-10-05 08:54:32 +1100

    Better handle the settings page

--- lib/models/admin_settings_page.rb
+++ lib/models/admin_settings_page.rb
@@ -2,12 +2,12 @@ class AdminSettingsPage < Page
   # TODO: when Site is changed to become a normal MongoRecord, this should
   # be replaced with a plain Page, no need for anything special
   respond_to :put do
-    with :json do
+    with :html do
       site.name = params['name']
-      site.idenfitier = params['identifier']
+      site.identifier = params['identifier']
       site.domains = params['domains']
       site.save
-      response.redirect_to '/admin/settings'
+      response.redirect '/admin/settings'
     end
   end
 end
